8b6912c9439d746f7128a8465c88c3b9dba4e3c3
Khofi-Adjei007 <khofiadjei@gmail.com>
2024-10-01 17:41:28 +0000

    Phase 1 : teachers page @ 60 done with

--- frankfurt_school_manager/settings.py
+++ frankfurt_school_manager/settings.py
@@ -79,7 +79,8 @@ ROOT_URLCONF = 'frankfurt_school_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'teachers_analytics')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
